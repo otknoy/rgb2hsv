b0c5d2ab7de81f0e6641ef162dcd3db6a9b17d76
otknoy <otknoy@gmail.com>
2013-11-02 04:20:20 +0900

    simplify

--- rgb2hsv.py
+++ rgb2hsv.py
@@ -9,29 +9,13 @@ imagename = "img/red_square.jpg"
 # 画像読み込み
 imagedata = cv2.imread(imagename)
 imageheight, imagewidth = imagedata.shape[:2]
-    
-the_number_of_pixel = imageheight * imagewidth
-
-# RGB の値
-r_value = imagedata[:,:,2]
-g_value = imagedata[:,:,1]
-b_value = imagedata[:,:,0]
 
 # GRBからHSVに変換
 hsvdata = cv2.cvtColor(imagedata, cv2.COLOR_BGR2HSV)
 
-# HSV の値
-h_value = hsvdata[:,:,0]
-s_value = hsvdata[:,:,1]
-v_value = hsvdata[:,:,2]
-
 # 各値の平均値
-mean_b = numpy.mean(b_value)
-mean_g = numpy.mean(g_value)
-mean_r = numpy.mean(r_value)
-mean_h = numpy.mean(h_value)
-mean_s = numpy.mean(s_value)
-mean_v = numpy.mean(v_value)
+mean_b, mean_g, mean_r = [e.mean() for e in imagedata.transpose()]
+mean_h, mean_s, mean_v = [e.mean() for e in hsvdata.transpose()]
 
 print "R = " + str(mean_r), "G = " + str(mean_g), "B = " + str(mean_b)
 print "H = " + str(mean_h), "S = " + str(mean_s), "V = " + str(mean_v)
